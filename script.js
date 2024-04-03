622fd24f2e93ae188d714abcf7c920159a274a33
Sai Prasad <148376552+SaiPrasad-22@users.noreply.github.com>
2024-04-03 19:33:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
-  // write your code here
+  let result = '';
+	const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz';
+	const charlength = char.length;
+
+	for(let i=0; i > l; i++){
+		result = result + char.charAt(Math.floor(Math.random() * charlength));
+	} return result;
 }
+console.log(makeid(6));
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
